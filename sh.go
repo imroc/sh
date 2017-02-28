f0855b1b1e014764a1275ee6b4f97bb79b8c96c9
imroc <imrocchan@gmail.com>
2017-02-28 22:59:39 +0800

    add sh.Int and doc

--- sh.go
+++ sh.go
@@ -1,20 +1,38 @@
 package sh
 
-import "os/exec"
+import (
+	"os/exec"
+	"strconv"
+	"strings"
+)
 
+// Run just execute the script.
 func Run(script string) error {
 	return exec.Command("sh", "-c", script).Run()
 }
 
+// Output execute the script, and receive the output as []byte.
 func Output(script string) ([]byte, error) {
 	return exec.Command("sh", "-c", script).Output()
 }
 
+// String execute the script, and receive the output as string.
 func String(script string) (result string, err error) {
 	bs, err := Output(script)
 	if err != nil {
 		return
 	}
 	result = string(bs)
+	return
+}
+
+// Int execute the script, and receive the output as int.
+func Int(script string) (result int, err error) {
+	s, err := String(script)
+	if err != nil {
+		return
+	}
+	s = strings.TrimSpace(s)
+	result, err = strconv.Atoi(s)
 	return
 }
